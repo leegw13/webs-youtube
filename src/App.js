ee2c8c001c1a33333f9a15d6899e09994385e8dc
leegw13 <sineszxc123@naver.com>
2024-01-18 01:00:40 +0900

    섹션 컴포넌트 구조화하기

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
+// Pages
 import Home from './pages/Home'
 import Today from './pages/Today'
 import Developer from './pages/Developer'
@@ -14,25 +15,33 @@ import Video from './pages/Video'
 import Search from './pages/Search'
 import Not from './pages/Not'
 
+// Components
+import Header from './components/section/Header';
+import Main from './components/section/Main';
+import Footer from './components/section/Footer';
+
 const App = () => {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path='/' element={<Home />} />
-                <Route path='/today' element={<Today />} />
-                <Route path='/developer' element={<Developer />} />
-                <Route path='/webd' element={<Webd />} />
-                <Route path='/website' element={<Website />} />
-                <Route path='/gsap' element={<Gsap />} />
-                <Route path='/port' element={<Port />} />
-                <Route path='/youtube' element={<Youtube />} />
-                <Route path='/channel/:channelId' element={<Channel />} />
-                <Route path='/video/:videoId' element={<Video />} />
-                <Route path='/search/:searchId' element={<Search />} />
-                <Route path='*' element={<Not />} />
-            </Routes>
-        </BrowserRouter>
-    );
+  return (
+    <BrowserRouter>
+      <Header />
+      <Main />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/today' element={<Today />} />
+        <Route path='/developer' element={<Developer />} />
+        <Route path='/webd' element={<Webd />} />
+        <Route path='/website' element={<Website />} />
+        <Route path='/gsap' element={<Gsap />} />
+        <Route path='/port' element={<Port />} />
+        <Route path='/youtube' element={<Youtube />} />
+        <Route path='/channel/:channelId' element={<Channel />} />
+        <Route path='/video/:videoId' element={<Video />} />
+        <Route path='/search/:searchId' element={<Search />} />
+        <Route path='*' element={<Not />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  );
 }
 
 export default App;
